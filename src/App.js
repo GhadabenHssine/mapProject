76ed320bc8c644061b62cffa6ac61fe3ad554367
ghada <ghadabenhssine69@gmail.com>
2023-07-17 17:36:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import "./App.css"
+import icon from "./img/téléchargement-removebg-preview.png"
+import l from "leaflet"
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js"
+import LeafletGeocoder from './LeafletGeocoder'
 
-function App() {
+const App = () => {
+  const position = [36.8065, 10.1815]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LeafletGeocoder />
+      </MapContainer>
     </div>
-  );
+  )
 }
+let DefaultIcon = l.icon({
+  iconUrl: icon,
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40]
+})
+l.Marker.prototype.options.icon = DefaultIcon;
 
-export default App;
+export default App
